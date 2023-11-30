112334ae977bf5b92ba17b35f76f5d9e6fc8e1be
NafisaKaruri <nafeesahassan016@gmail.com>
2023-11-30 09:42:29 -0800

    updated

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-upper_alphabet = map(chr, range(ord('A'), ord('Z')+1))
-print(*upper_alphabet, sep='', end='\n')
+import fast_alphabet
---
+++ 0x02-python-import_modules/fast_alphabet.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for c in range(ord("A"), ord("Z") + 1):
+    print("{:c}".format(c), end='')
+print('')
