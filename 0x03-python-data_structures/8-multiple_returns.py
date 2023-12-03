b69068771e2df44d3b239adefcfbadfb74187ecc
NafisaKaruri <nafeesahassan016@gmail.com>
2023-12-03 04:40:52 -0800

    updated

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence == '':
-        return len((None, )), None
+        return 0, None
     tup = tuple(sentence)
     return len(tup), tup[0]
