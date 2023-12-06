82ee7890a8294c870858255e0fc937173cfa0ae1
NafisaKaruri <nafeesahassan016@gmail.com>
2023-12-06 15:08:44 -0800

    added 100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list == '':
+        return 0
+    numerator = 0
+    denominator = 0
+    for i, j in my_list:
+        numerator += i * j
+        denominator += j
+    return numerator/denominator
