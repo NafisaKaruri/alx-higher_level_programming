c8bfd71f0e4f46f67f50a053db7a934c53f36492
NafisaKaruri <nafeesahassan016@gmail.com>
2023-12-06 14:09:49 -0800

    fixed a bug

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    if a_dictionary is None:
+        return
     sd = sorted(a_dictionary.keys())
     for key in sd:
         print("{}: {}".format(key, a_dictionary.get(key)))
