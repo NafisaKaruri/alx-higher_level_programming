d8b965b8c13443f654051371583770a0bce3d059
NafisaKaruri <nafeesahassan016@gmail.com>
2023-12-19 13:42:54 -0800

    added 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Defines a class Square."""
+
+
+class Square:
+    """
+    Class that defines a square.
+
+    Attributes:
+        size: size of the square.
+    """
+    def __init__(self, size=0):
+        """
+        Create a new instanse of the square.
+        Args:
+            size: size of the square.
+        """
+        self.__size = size
+
+    def area(self):
+        """
+        Calculate the area of the square.
+
+        Returns: the current square area.
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Returns the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for size.
+
+        Args:
+            value (int): size of the square.
+
+        Raises:
+            TypeError: size must be an integer
+            ValueError: size must be >= 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
