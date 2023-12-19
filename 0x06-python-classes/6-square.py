34e1d3360080b0715be5235cb684c1853c289933
NafisaKaruri <nafeesahassan016@gmail.com>
2023-12-19 14:25:43 -0800

    updated

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -17,8 +17,8 @@ class Square:
             size (int): size of the square.
             position (tuple): position of the square.
         """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     def area(self):
         """
