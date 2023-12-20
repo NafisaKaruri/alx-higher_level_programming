341699c034d2e6eb07c7bbb6c2eb8c7cad0a153d
NafisaKaruri <nafeesahassan016@gmail.com>
2023-12-19 16:38:22 -0800

    updated 103-magic_class.py

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -27,7 +27,7 @@ class MagicClass:
         """
         Returns: the area of a circle.
         """
-        return (self.__radius ** 2) * math.pi
+        return math.pi * self.__radius ** 2
 
     def circumference(self):
         """
