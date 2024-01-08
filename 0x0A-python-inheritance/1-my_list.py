eb12f3dba7ba9e17a4e69df4b2845673c3445a35
NafisaKaruri <nafeesahassan016@gmail.com>
2024-01-08 13:34:56 -0800

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class MyList"""
+
+
+class MyList(list):
+    """Class with a print_sorted method"""
+
+    def print_sorted(self):
+        """Sorts and prints a list"""
+        print(sorted(list(self)))
