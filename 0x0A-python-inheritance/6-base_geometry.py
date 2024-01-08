51cc2bfbe898aafcdfac6f67a1edba0de9ffd096
NafisaKaruri <nafeesahassan016@gmail.com>
2024-01-08 14:24:19 -0800

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines BaseGeometry class"""
+
+
+class BaseGeometry:
+    """Defines a public instance method area"""
+
+    def area(self):
+        """raises: area() is not implemented"""
+        raise Exception("area() is not implemented")
