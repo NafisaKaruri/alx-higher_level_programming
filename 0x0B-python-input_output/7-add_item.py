de4117e51583bee1567befbd83a44985cf02bc90
NafisaKaruri <nafeesahassan016@gmail.com>
2024-01-09 14:34:29 -0800

    task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""""""
+
+
+import json
+import os.path
+from sys import argv
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = 'add_item.json'
+lst = []
+
+if os.path.exists(filename):
+    lst = load_from_json_file(filename)
+
+for i in argv[1:]:
+    lst.append(i)
+
+save_to_json_file(lst, filename)
