e5d1cf84935bbe2fbf03e81dd1ed4aa3675c9866
NafisaKaruri <nafeesahassan016@gmail.com>
2024-01-08 17:30:37 -0800

    task 3

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Defines the print_square method"""
+
+
 def print_square(size):
+    """prints a square with #"""
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
