e84ee0baf5dc38026ea845235b987a54a7a909f3
NafisaKaruri <nafeesahassan016@gmail.com>
2024-01-15 15:51:53 -0800

    mandatory

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,10 @@
 """Base class"""
 
 
+from json import dumps, loads
+import csv
+
+
 class Base:
     """The base of our hierarchy"""
     __nb_objects = 0
@@ -13,3 +17,51 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictioinaries"""
+        if not list_dictionaries or list_dictionaries is None:
+            return '[]'
+        else:
+            return dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes JSON string to file"""
+        if list_objs is not None:
+            list_objs = [o.to_dictionary() for o in list_objs]
+        with open('{}.json'.format(cls.__name__), 'w', encoding='utf_8') as f:
+            f.write(cls.to_json_string(list_objs))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of JSON string representation of json_string"""
+        if not json_string or json_string is None:
+            return []
+        else:
+            return loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """return an instance with all attributes set"""
+        from models.rectangle import Rectangle
+        from models.square import Square
+        if cls is Rectangle:
+            created = Rectangle(1, 1)
+        elif cls is Square:
+            created = Square(1)
+        else:
+            created = None
+        created.update(**dictionary)
+        return created
+
+    @classmethod
+    def load_from_file(cls):
+        """returns a list of instances"""
+        from os import path
+        file = '{}.json'.format(cls.__name__)
+        if not path.isfile(file):
+            return []
+        with open(file, 'r', encoding='utf_8')as f:
+            return [cls.create(**d) for d in cls.from_json_string(f.read())]
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -5,6 +5,7 @@
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
+from models.square import Square
 
 
 class TestBase(unittest.TestCase):
@@ -78,3 +79,163 @@ class TestBase(unittest.TestCase):
         """test id with keyword"""
         b = Base(id=100)
         self.assertEqual(b.id, 100)
+
+    def test_to_json_string(self):
+        """test to_json_string method"""
+        with self.assertRaises(TypeError) as e:
+            Base.to_json_string()
+        err = "to_json_string() missing 1 required positional argument: \
+'list_dictionaries'"
+        self.assertEqual(str(e.exception), err)
+
+        self.assertEqual(Base.to_json_string(None), '[]')
+        self.assertEqual(Base.to_json_string([]), '[]')
+
+        d = [{"x": 2, "width": 10, "id": 1, "height": 7, "y": 8}]
+        self.assertEqual(len(Base.to_json_string(d)), len(str(d)))
+
+        d = [{"hello": 1}]
+        self.assertEqual(Base.to_json_string(d), '[{"hello": 1}]')
+
+        d = [{"hello": 1}, {"world": 2}]
+        self.assertEqual(Base.to_json_string(d),
+                         '[{"hello": 1}, {"world": 2}]')
+
+        d = [{"x": 2, "width": 10, "id": 1, "height": 7, "y": 8},
+             {"x": 25, "width": 13, "id": 10, "height": 347, "y": 34}]
+        self.assertEqual(len(Base.to_json_string(d)), len(str(d)))
+
+        d = [{}, {}]
+        self.assertEqual(Base.to_json_string(d), '[{}, {}]')
+
+        r = Rectangle(1, 2, 3, 4)
+        d = r.to_dictionary()
+        json_d = Base.to_json_string([d])
+        d = str([d])
+        self.assertEqual(d.replace("'", '"'), json_d)
+
+        r1 = Rectangle(1, 2, 3, 4)
+        r2 = Rectangle(5, 6, 7, 8)
+        d = [r1.to_dictionary(), r2.to_dictionary()]
+        json_d = Base.to_json_string([d])
+        d = str([d])
+        self.assertEqual(d.replace("'", '"'), json_d)
+
+        r = Square(1, 2, 3)
+        d = r.to_dictionary()
+        json_d = Base.to_json_string([d])
+        d = str([d])
+        self.assertEqual(d.replace("'", '"'), json_d)
+
+        r1 = Square(1, 2, 3)
+        r2 = Square(4, 5, 6)
+        d = [r1.to_dictionary(), r2.to_dictionary()]
+        json_d = Base.to_json_string([d])
+        d = str([d])
+        self.assertEqual(d.replace("'", '"'), json_d)
+
+    def test_save_to_file(self):
+        """test save_to_file"""
+        import os
+        r1 = Rectangle(1, 2, 3, 4)
+        r2 = Rectangle(5, 6)
+        Rectangle.save_to_file([r1, r2])
+        with open("Rectangle.json", 'r') as f:
+            self.assertEqual(len(f.read()), 104)
+
+        Rectangle.save_to_file(None)
+        with open("Rectangle.json", 'r') as f:
+            self.assertEqual(f.read(), "[]")
+
+        try:
+            os.remove("Rectangle.json")
+        except:
+            pass
+        Rectangle.save_to_file([])
+        with open("Rectangle.json", 'r') as f:
+            self.assertEqual(f.read(), "[]")
+
+        r = Rectangle(1, 2)
+        Rectangle.save_to_file([r])
+        with open("Rectangle.json", 'r') as f:
+            self.assertEqual(len(f.read()), 52)
+
+        Square.save_to_file(None)
+        with open("Square.json", 'r') as f:
+            self.assertEqual(f.read(), "[]")
+
+        try:
+            os.remove("Square.json")
+        except:
+            pass
+        Square.save_to_file([])
+        with open("Square.json", 'r') as f:
+            self.assertEqual(f.read(), "[]")
+
+        r = Square(1)
+        Square.save_to_file([r])
+        with open("Square.json", 'r') as f:
+            self.assertEqual(len(f.read()), 38)
+
+    def test_from_json(self):
+        """test from_json_string"""
+        with self.assertRaises(TypeError) as e:
+            Base.from_json_string()
+        err = "from_json_string() missing 1 required positional argument\
+: 'json_string'"
+        self.assertEqual(str(e.exception), err)
+        self.assertEqual(Base.from_json_string(None), [])
+        self.assertEqual(Base.from_json_string(''), [])
+
+        d = [{"x": 1, "y": 2, "width": 3, "id": 4, "height": 5},
+             {"x": 10, "y": 20, "width": 30, "id": 40, "height": 50}]
+        s = '[{"x": 1, "y": 2, "width": 3, "id": 4, "height": 5},\
+{"x": 10, "y": 20, "width": 30, "id": 40, "height": 50}]'
+        self.assertEqual(Base.from_json_string(s), d)
+
+        d = [{}, {}]
+        s = '[{}, {}]'
+        self.assertEqual(Base.from_json_string(s), d)
+
+        d = [{}]
+        s = '[{}]'
+        self.assertEqual(Base.from_json_string(s), d)
+
+        d = [{"eh": 1}]
+        s = '[{"eh": 1}]'
+        self.assertEqual(Base.from_json_string(s), d)
+
+    def test_create(self):
+        """test create"""
+        r = Rectangle(1, 2, 3)
+        d = r.to_dictionary()
+        r1 = Rectangle.create(**d)
+        self.assertEqual(str(r), str(r1))
+        self.assertFalse(r == r1)
+        self.assertFalse(r is r1)
+
+    def test_load_from_file(self):
+        """test load_from_file"""
+        r1 = Rectangle(1, 2, 3, 4)
+        r2 = Rectangle(5, 6)
+        f = [r1, r2]
+        Rectangle.save_to_file(f)
+        output = Rectangle.load_from_file()
+        self.assertNotEqual(id(f[0]), id(output[0]))
+        self.assertNotEqual(id(f[1]), id(output[1]))
+        self.assertEqual(str(f[0]), str(output[0]))
+        self.assertEqual(str(f[1]), str(output[1]))
+
+        r1 = Square(1)
+        r2 = Square(2, 3, 4)
+        f = [r1, r2]
+        Square.save_to_file(f)
+        output = Square.load_from_file()
+        self.assertNotEqual(id(f[0]), id(output[0]))
+        self.assertNotEqual(id(f[1]), id(output[1]))
+        self.assertEqual(str(f[0]), str(output[0]))
+        self.assertEqual(str(f[1]), str(output[1]))
+
+
+if __name__ == "__main__":
+    unittest.main()
