e965f837f947805f4aefdfc0028a4c1897bcd3d6
NafisaKaruri <nafeesahassan016@gmail.com>
2024-02-13 12:07:27 -0800

    :)

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -12,6 +12,6 @@ COLLATE utf8mb4_unicode_ci;
 
 -- alter the name field
 ALTER TABLE first_table
-MODIFY name VARCHAR(256)
+CHANGE name name VARCHAR(256)
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
