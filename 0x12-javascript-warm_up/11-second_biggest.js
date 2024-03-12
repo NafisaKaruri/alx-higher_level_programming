677f1f866da86dda48458e03b247f4602e1652bb
NafisaKaruri <nafeesahassan016@gmail.com>
2024-03-11 17:22:14 -0700

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,6 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const list = process.argv.sort();
-  console.log(list.reverse()[1]);
+  const list = process.argv.slice(2).map(Number); // Convert arguments to numbers
+  console.log(list.sort((a, b) => b - a)[1]); // Get the second largest number
 }
