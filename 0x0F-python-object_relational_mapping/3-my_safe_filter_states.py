9b75e8ab4b23b6c36ed4f1f670b32efc663d2b6c
NafisaKaruri <nafeesahassan016@gmail.com>
2024-03-16 17:50:43 -0700

    task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""List all states with a name matching argument from the database"""
+
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3],
+                           charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states \
+                WHERE name LIKE BINARY '{}'".format(argv[4]))
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    conn.close()
