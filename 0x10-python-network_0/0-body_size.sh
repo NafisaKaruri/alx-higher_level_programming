d5e873c06f1376e7e445690afe278f93ccc4c8b0
NafisaKaruri <nafeesahassan016@gmail.com>
2024-03-27 17:39:14 -0700

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# Takes in a URL,
-# sends a request to it,
-# and display the size of the body of the response
-# Usage: ./0-body_size.sh URL
-
+# Takes in a URL, sends a request to it, and display the size of the body of the response
 curl -s "${1}" | wc -c
