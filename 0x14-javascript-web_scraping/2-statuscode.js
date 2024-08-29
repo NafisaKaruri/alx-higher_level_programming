b3153ebba5053a83187d434eda711896880ecfa5
NafisaKaruri <nafeesahassan016@gmail.com>
2024-08-29 08:52:03 -0700

    2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 
 const request = require('request');
+const url = process.argv[2];
 
-request(process.argv[2], function(err, response, body) {
+request(url, function(err, response, body) {
   if (err) {
     console.log(err);
   } else {
